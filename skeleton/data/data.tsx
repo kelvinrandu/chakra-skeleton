d0d98b34c6bda297c2f3e477959be660db0fdd53
kelvinrandu <randukelvin@gmail.com>
2022-08-06 09:18:57 +0300

    added properties layout

---
+++ skeleton/data/data.tsx
@@ -0,0 +1,30 @@
+export const skills = [
+  {
+    name: "React",
+    description: "front-end stuff",
+    image: "",
+    link: "",
+    platform: "",
+  },
+  {
+    name: "Hasura",
+    description: "backend-end stuff",
+    image: "",
+    link: "",
+    platform: "",
+  },
+  {
+    name: "Git",
+    description: "Version control stuff",
+    image: "",
+    link: "",
+    platform: "",
+  },
+  {
+    name: "aws",
+    description: "Server stuff",
+    image: "",
+    link: "",
+    platform: "",
+  },
+];
--- skeleton/next.config.js
+++ skeleton/next.config.js
@@ -1,12 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 module.exports = {
   images: {
     domains: ["bit.ly"],
   },
 };
 
-module.exports = nextConfig
+// module.exports = nextConfig
